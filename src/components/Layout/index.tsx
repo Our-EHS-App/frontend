70f956a9a5f27a35bbd82f4c2a3377168a482996
almeshekah <77221864+almeshekah@users.noreply.github.com>
2023-09-30 19:14:41 +0300

    Add sort

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -130,7 +130,10 @@ export const MainLayout: FC = () => {
                 leave='transition ease-in-out duration-300 transform'
                 leaveFrom='translate-x-0'
                 leaveTo='-translate-x-full'>
-                <Dialog.Panel className='relative mr-16 flex w-full max-w-xs flex-1'>
+                <Dialog.Panel
+                  className={`relative ${
+                    language === 'ar' ? `` : `mr-16`
+                  } flex w-full max-w-xs flex-1`}>
                   <Transition.Child
                     as={Fragment}
                     enter='ease-in-out duration-300'
@@ -139,7 +142,10 @@ export const MainLayout: FC = () => {
                     leave='ease-in-out duration-300'
                     leaveFrom='opacity-100'
                     leaveTo='opacity-0'>
-                    <div className='absolute left-full top-0 flex w-16 justify-center pt-5'>
+                    <div
+                      className={`absolute ${
+                        language === 'ar' ? `right-full` : `left-full`
+                      }  top-0 flex w-16 justify-center pt-5`}>
                       <button
                         type='button'
                         className='-m-2.5 p-2.5 bg-transparent border-0'
