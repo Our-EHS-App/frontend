40c2548d5f9c75349e19f4ddae8aff6a5cdc0328
almeshekah <77221864+almeshekah@users.noreply.github.com>
2023-11-19 21:00:50 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     host: 'localhost',
     proxy: {
       '/api': {
-        target: 'https://emtithal-backend.up.railway.app',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       },
