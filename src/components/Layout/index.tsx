2001d6823e61a2b1ee19f6f5b483d7f8bb55f279
abdulazizfahad <abdulaziz.f.mw@gmail.com>
2023-11-29 09:49:06 +0300

    Hide admin in the profile

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -329,7 +329,7 @@ export const MainLayout: FC = () => {
                       <span
                         className='ml-4 text-sm font-semibold leading-6 text-gray-900'
                         aria-hidden='true'>
-                        Admin
+                        
                       </span>
                       <ChevronDownIcon
                         className='ml-2 h-5 w-5 text-gray-400'
